ac1427d1d7ad7f14b05d95506e5f594de735adae
Etativel <farhanmaulana4611@gmail.com>
2024-11-17 15:25:31 +0800

    feat: load response data as new data

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@ import "./App.css";
 import CardContainer from "./components/CardContainer";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import Loader from "./components/Loader";
+import { api } from "./services/api";
+import { shuffle } from "./utils/cardUtils";
 function App() {
   const [score, setScore] = useState(0);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const pokemonData = await api();
+      const shuffledData = shuffle(pokemonData);
+      setData(shuffledData);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="app-container">
       <Header score={score} />
-      <CardContainer setScore={setScore} score={score} />
+      <CardContainer setScore={setScore} score={score} data={data} />
       <Footer />
     </div>
   );
