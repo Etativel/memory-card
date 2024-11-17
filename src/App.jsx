0dfe901a02096aac05dd93cafcff88c40a0bf0fa
Etativel <farhanmaulana4611@gmail.com>
2024-11-17 15:53:47 +0800

    feat: track best score

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,43 @@ import "./App.css";
 import CardContainer from "./components/CardContainer";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Loader from "./components/Loader";
 import { useEffect, useState } from "react";
 // import Loader from "./components/Loader";
 import { api } from "./services/api";
-import { shuffle } from "./utils/cardUtils";
+
 function App() {
   const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState();
   const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (score < bestScore) return;
+    setBestScore(score);
+  }, [score]);
   useEffect(() => {
     async function fetchData() {
       const pokemonData = await api();
-      const shuffledData = shuffle(pokemonData);
-      setData(shuffledData);
+      setData(pokemonData);
     }
     fetchData();
   }, []);
 
   return (
     <div className="app-container">
-      <Header score={score} />
-      <CardContainer setScore={setScore} score={score} data={data} />
+      <Header score={score} bestScore={bestScore} />
+      {data.length === 0 ? (
+        <Loader />
+      ) : (
+        <CardContainer
+          setScore={setScore}
+          setBestScore={setBestScore}
+          score={score}
+          bestScore={bestScore}
+          data={data}
+        />
+      )}
+
       <Footer />
     </div>
   );
