8fd32215b1e8463f46a5b06a1d5e008d0a4ae41b
Etativel <farhanmaulana4611@gmail.com>
2024-11-17 10:38:12 +0800

    feat: create basic component

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,12 @@
+import "../styles/CardContainer.css";
+
+export default function CardContainer() {
+  return (
+    <div className="card-container">
+      <div className="card-1 card">aasdf</div>
+      <div className="card-2 card">asdf</div>
+      <div className="card-3 card">asdf</div>
+      <div className="card-4 card">asdf</div>
+    </div>
+  );
+}
